3c635b0161b20198d9240096c8128978ac863187
walter-eidi <walter-eidi@hotmail.com>
2025-03-02 19:59:07 -0300

    Adicionar arquivos de projeto.

---
+++ DLLStore/Store.cs
@@ -0,0 +1,22 @@
+﻿using Framework1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLStore
+{
+    public class Store
+    {
+        public Store()
+        {
+            new Testes();
+        }
+        public Testes TesteOutraRef()
+        {
+            return new Testes();
+        }
+
+    }
+}
---
+++ Framework1/Testes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework1
+{
+    public class Testes
+    {
+        public Testes()
+        {
+
+        }
+        public int GetSomething()
+        {
+            return 2;
+        }
+
+    }
+}
